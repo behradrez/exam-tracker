57ab3233ace1362b226f1e9b52ef50f22930c182
Behrad Rezaie <r.behrad121@gmail.com>
2025-01-26 17:19:48 -0500

    type adjustment

--- src/types/exam.ts
+++ src/types/exam.ts
@@ -3,11 +3,11 @@ export type Exam = {
     created_at: Date;
     exam_start: Date | null;
     exam_end: Date | null;
-    course_code: string | null;
+    course_code: string;
     course_name: string | null;
     exam_type: string | null;
     building: string | null;
     room: string | null;
     rows: string | null;
-    section: String | null;
+    section: String;
 };
